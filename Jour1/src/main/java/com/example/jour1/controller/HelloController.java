e9c6d54d58b8229e4e760a18214f3188663581c7
Elie <elie.aigon@laplateforme.io>
2023-09-20 10:40:17 +0200

    jour1

---
+++ Jour1/src/main/java/com/example/jour1/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.jour1.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${mon.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String getMessage() {
+        return message;
+    }
+}
